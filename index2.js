393d80ff902c6048cdebf249e80d2c9d83781516
unknown <Fanor@FDN-WS-023535.acad.dvuadmin.net>
2018-01-26 22:09:22 -0300

    index2

---
+++ index2.js
@@ -0,0 +1,20 @@
+const CryptoJS = require("crypto-js");
+
+/*
+ calcule uma operação de cifra em cima de um argumento recebido dependendo do valor de uma variável de ambiente. 
+ Se a variável ALG for igual a MD5, use o algoritmo MD5. Se não houver a variável ALG, 
+ use o algoritmo SHA256 visto no exemplo.
+ */
+if (process.argv.length < 3){
+    console.log('usage: node index2.js string')
+    return
+}
+var word = process.argv[2];
+if (process.env.ALG && process.env.ALG == "MD5") {
+    word = CryptoJS.MD5(word).toString(CryptoJS.enc.Base64);
+} else {
+    word = CryptoJS.SHA256(word).toString(CryptoJS.enc.Base64);
+}
+console.log(word);
+
+
